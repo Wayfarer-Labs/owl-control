85146c15b871e8a055b0406725b5df7f4fc1f8fe
Philpax <me@philpax.me>
2025-09-26 21:23:54 +0200

    fix #70 - use both xinput/wgi gilrs to capture PS controllers

--- crates/input-capture/src/gamepad_capture.rs
+++ crates/input-capture/src/gamepad_capture.rs
@@ -1,3 +1,8 @@
+use std::{
+    collections::HashSet,
+    sync::{Arc, RwLock},
+};
+
 use tokio::sync::mpsc;
 
 use crate::{Event, PressState};
@@ -39,92 +44,143 @@ pub const AXIS_DPADX: u16 = 7;
 pub const AXIS_DPADY: u16 = 8;
 
 pub fn initialize_thread(input_tx: mpsc::Sender<Event>) -> std::thread::JoinHandle<()> {
-    std::thread::spawn(move || {
-        let mut gilrs = gilrs::Gilrs::new().unwrap();
-        let mut active_gamepad = None;
+    let already_captured_by_xinput = Arc::new(RwLock::new(HashSet::new()));
 
-        // Examine new events
-        while let Some(gilrs::Event { id, event, .. }) = gilrs.next_event_blocking(None) {
-            if active_gamepad.is_none() {
-                active_gamepad = Some(id);
+    // We use both the `xinput` and `wgi` versions of gilrs so that we can capture Xbox controllers
+    // (which only work with `xinput`) and PS controllers (which only work with `wgi`).
+    //
+    // We should ostensibly be able to use just `wgi`, but `wgi` only supports all controllers with a focused window
+    // associated with the process, which we can't do for our delightful little input recorder.
+    // However, it *does* work without a focused window for PS controllers.
+    //
+    // I love Windows.
+
+    // xinput
+    std::thread::spawn({
+        let already_captured_by_xinput = already_captured_by_xinput.clone();
+        let input_tx = input_tx.clone();
+        move || {
+            let mut gilrs = gilrs_xinput::Gilrs::new().unwrap();
+
+            // Examine new events
+            while let Some(gilrs_xinput::Event { id, event, .. }) = gilrs.next_event_blocking(None)
+            {
+                let name = gilrs.gamepad(id).name().to_string();
+                already_captured_by_xinput.write().unwrap().insert(name);
+
+                let Some(event) = map_event_xinput(event) else {
+                    continue;
+                };
+                if input_tx.blocking_send(event).is_err() {
+                    tracing::warn!("Gamepad input tx closed, stopping gamepad capture");
+                    break;
+                }
             }
+        }
+    });
 
-            let Some(event) = map_event(event) else {
-                continue;
-            };
-            if input_tx.blocking_send(event).is_err() {
-                tracing::warn!("Gamepad input tx closed, stopping gamepad capture");
-                break;
+    // wgi
+    std::thread::spawn({
+        let already_captured_by_xinput = already_captured_by_xinput.clone();
+        move || {
+            let mut gilrs = gilrs_wgi::Gilrs::new().unwrap();
+
+            // Examine new events
+            while let Some(gilrs_wgi::Event { id, event, .. }) = gilrs.next_event_blocking(None) {
+                let name = gilrs.gamepad(id).name().to_string();
+                if already_captured_by_xinput.read().unwrap().contains(&name) {
+                    continue;
+                }
+
+                let Some(event) = map_event_wgi(event) else {
+                    continue;
+                };
+                if input_tx.blocking_send(event).is_err() {
+                    tracing::warn!("Gamepad input tx closed, stopping gamepad capture");
+                    break;
+                }
             }
         }
     })
 }
 
-fn map_event(event: gilrs::EventType) -> Option<Event> {
-    use gilrs::EventType;
-    match event {
-        EventType::ButtonPressed(button, _) => Some(Event::GamepadButtonPress {
-            key: map_button(button),
-            press_state: PressState::Pressed,
-        }),
-        EventType::ButtonReleased(button, _) => Some(Event::GamepadButtonPress {
-            key: map_button(button),
-            press_state: PressState::Released,
-        }),
-        EventType::ButtonChanged(button, value, _) => Some(Event::GamepadButtonChange {
-            key: map_button(button),
-            value,
-        }),
-        EventType::AxisChanged(axis, value, _) => Some(Event::GamepadAxisChange {
-            axis: map_axis(axis),
-            value,
-        }),
-        EventType::ButtonRepeated(..)
-        | EventType::Connected
-        | EventType::Disconnected
-        | EventType::Dropped
-        | EventType::ForceFeedbackEffectCompleted
-        | _ => None,
-    }
+macro_rules! generate_map_functions {
+    ($gilrs:ident, $map_event:ident, $map_button:ident, $map_axis:ident) => {
+        fn $map_event(event: $gilrs::EventType) -> Option<Event> {
+            use $gilrs::EventType;
+            match event {
+                EventType::ButtonPressed(button, _) => Some(Event::GamepadButtonPress {
+                    key: $map_button(button),
+                    press_state: PressState::Pressed,
+                }),
+                EventType::ButtonReleased(button, _) => Some(Event::GamepadButtonPress {
+                    key: $map_button(button),
+                    press_state: PressState::Released,
+                }),
+                EventType::ButtonChanged(button, value, _) => Some(Event::GamepadButtonChange {
+                    key: $map_button(button),
+                    value,
+                }),
+                EventType::AxisChanged(axis, value, _) => Some(Event::GamepadAxisChange {
+                    axis: $map_axis(axis),
+                    value,
+                }),
+                EventType::ButtonRepeated(..)
+                | EventType::Connected
+                | EventType::Disconnected
+                | EventType::Dropped
+                | EventType::ForceFeedbackEffectCompleted
+                | _ => None,
+            }
+        }
+
+        fn $map_button(button: $gilrs::Button) -> u16 {
+            use $gilrs::Button;
+            match button {
+                Button::South => BTN_SOUTH,
+                Button::East => BTN_EAST,
+                Button::North => BTN_NORTH,
+                Button::West => BTN_WEST,
+                Button::C => BTN_C,
+                Button::Z => BTN_Z,
+                Button::LeftTrigger => BTN_LT,
+                Button::LeftTrigger2 => BTN_LT2,
+                Button::RightTrigger => BTN_RT,
+                Button::RightTrigger2 => BTN_RT2,
+                Button::Select => BTN_SELECT,
+                Button::Start => BTN_START,
+                Button::Mode => BTN_MODE,
+                Button::LeftThumb => BTN_LTHUMB,
+                Button::RightThumb => BTN_RTHUMB,
+                Button::DPadUp => BTN_DPAD_UP,
+                Button::DPadDown => BTN_DPAD_DOWN,
+                Button::DPadLeft => BTN_DPAD_LEFT,
+                Button::DPadRight => BTN_DPAD_RIGHT,
+                Button::Unknown => BTN_UNKNOWN,
+            }
+        }
+
+        fn $map_axis(axis: $gilrs::Axis) -> u16 {
+            use $gilrs::Axis;
+            match axis {
+                Axis::LeftStickX => AXIS_LSTICKX,
+                Axis::LeftStickY => AXIS_LSTICKY,
+                Axis::LeftZ => AXIS_LEFTZ,
+                Axis::RightStickX => AXIS_RSTICKX,
+                Axis::RightStickY => AXIS_RSTICKY,
+                Axis::RightZ => AXIS_RIGHTZ,
+                Axis::DPadX => AXIS_DPADX,
+                Axis::DPadY => AXIS_DPADY,
+                Axis::Unknown => AXIS_UNKNOWN,
+            }
+        }
+    };
 }
 
-fn map_button(button: gilrs::Button) -> u16 {
-    use gilrs::Button;
-    match button {
-        Button::South => BTN_SOUTH,
-        Button::East => BTN_EAST,
-        Button::North => BTN_NORTH,
-        Button::West => BTN_WEST,
-        Button::C => BTN_C,
-        Button::Z => BTN_Z,
-        Button::LeftTrigger => BTN_LT,
-        Button::LeftTrigger2 => BTN_LT2,
-        Button::RightTrigger => BTN_RT,
-        Button::RightTrigger2 => BTN_RT2,
-        Button::Select => BTN_SELECT,
-        Button::Start => BTN_START,
-        Button::Mode => BTN_MODE,
-        Button::LeftThumb => BTN_LTHUMB,
-        Button::RightThumb => BTN_RTHUMB,
-        Button::DPadUp => BTN_DPAD_UP,
-        Button::DPadDown => BTN_DPAD_DOWN,
-        Button::DPadLeft => BTN_DPAD_LEFT,
-        Button::DPadRight => BTN_DPAD_RIGHT,
-        Button::Unknown => BTN_UNKNOWN,
-    }
-}
-
-fn map_axis(axis: gilrs::Axis) -> u16 {
-    use gilrs::Axis;
-    match axis {
-        Axis::LeftStickX => AXIS_LSTICKX,
-        Axis::LeftStickY => AXIS_LSTICKY,
-        Axis::LeftZ => AXIS_LEFTZ,
-        Axis::RightStickX => AXIS_RSTICKX,
-        Axis::RightStickY => AXIS_RSTICKY,
-        Axis::RightZ => AXIS_RIGHTZ,
-        Axis::DPadX => AXIS_DPADX,
-        Axis::DPadY => AXIS_DPADY,
-        Axis::Unknown => AXIS_UNKNOWN,
-    }
-}
+generate_map_functions!(
+    gilrs_xinput,
+    map_event_xinput,
+    map_button_xinput,
+    map_axis_xinput
+);
+generate_map_functions!(gilrs_wgi, map_event_wgi, map_button_wgi, map_axis_wgi);
