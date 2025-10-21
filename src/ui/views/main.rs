5c9e8fa5c031f470b3540abbb289a0ac112b6720
Philpax <me@philpax.me>
2025-10-21 21:19:31 +0200

    feat: add tooltip to encoder

--- src/ui/views/main.rs
+++ src/ui/views/main.rs
@@ -274,9 +274,13 @@ impl MainApp {
                                     }
                                 });
 
-                            if ui.button("⚙ Settings").clicked() {
-                                self.encoder_settings_window_open = true;
-                            }
+                            ui.horizontal(|ui| {
+                                if ui.button("⚙ Settings").clicked() {
+                                    self.encoder_settings_window_open = true;
+                                }
+
+                                tooltip(ui, "Consider switching encoders and/or using a different preset if your recordings suffer from dropped frames. NVENC is known to drop frames when the GPU is under heavy load or does not have enough VRAM.", None)
+                            });
                         });
                     });
                 });
