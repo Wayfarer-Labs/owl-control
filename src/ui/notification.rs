84d740ab3af167bf510fa7a28ed523d0f1189c64
Clyde013 <40514241+Clyde013@users.noreply.github.com>
2025-11-10 21:35:12 +0800

    chore: cargo fmt

--- src/ui/notification.rs
+++ src/ui/notification.rs
@@ -1,5 +1,5 @@
 use windows::{
-    Win32::UI::WindowsAndMessaging::{MB_ICONERROR, MB_TOPMOST, MB_SETFOREGROUND, MessageBoxW},
+    Win32::UI::WindowsAndMessaging::{MB_ICONERROR, MB_SETFOREGROUND, MB_TOPMOST, MessageBoxW},
     core::HSTRING,
 };
 
